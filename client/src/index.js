a52f9eb3af9588d92c13efa84406ffb254d7bf50
Hussein Samy <hussein.samy02@gmail.com>
2023-06-17 22:21:24 +0200

    Adds more routes, toast container and axios config

--- client/src/index.js
+++ client/src/index.js
@@ -8,18 +8,47 @@ import MainHeaderLayout from './Layout/main-header.component';
 import Home from './pages/home/home.component';
 import Register from './pages/register/register.component';
 
+import axios from 'axios'
+import UserContextProvider from './contexts/users.context';
+import Account from './pages/account/account.component';
+import Places from './pages/places/places.component';
+import Profile from './pages/profile/profile.component';
+import PlacesForm from './pages/places-form/places-form.component';
+import Place from './pages/place/place.component';
+import Bookings from './pages/bookings/bookings.component';
+import Booking from './pages/booking/booking.component';
+
+
+import { ToastContainer } from 'react-toastify'
+
+axios.defaults.baseURL = 'http://localhost:4000/api';
+axios.defaults.withCredentials = true;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainHeaderLayout/>}>
-          <Route index element={<Home/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Route>
-      </Routes>
+      <UserContextProvider>
+      <ToastContainer/>
+        <Routes>
+          <Route path="/" element={<MainHeaderLayout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="/home" element={<Home/>}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/register" element={<Register />}/>
+            <Route path="/account" element={<Account />}>
+              <Route index element={<Profile />}/>
+              <Route path="places" element={<Places />}/>
+              <Route path="places/new" element={<PlacesForm />} />
+              <Route path="places/:id" element={<PlacesForm />} />
+            </Route>
+            <Route path="/place/:id" element={<Place />}/>
+            <Route path="/account/bookings" element={<Bookings />} />
+            <Route path="/account/bookings/:id" element={<Booking />} />
+          </Route>
+        </Routes>
+    </UserContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
