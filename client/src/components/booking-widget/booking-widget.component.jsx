2dc75528685b7e02b8fbf7ba8df6c437425de861
Hussein Samy <hussein.samy02@gmail.com>
2023-06-18 17:42:21 +0200

    Adds booking-widget component

---
+++ client/src/components/booking-widget/booking-widget.component.jsx
@@ -0,0 +1,116 @@
+import {useContext, useEffect, useState} from "react";
+import {differenceInCalendarDays} from "date-fns";
+import axios from "axios";
+import {Navigate} from "react-router-dom";
+import { UserContext } from "../../contexts/users.context";
+import { toast } from 'react-toastify'
+
+
+export default function BookingWidget({place}) {
+  const [checkIn,setCheckIn] = useState('');
+  const [checkOut,setCheckOut] = useState('');
+  const [numberOfGuests,setNumberOfGuests] = useState(1);
+  const [name,setName] = useState('');
+  const [phone,setPhone] = useState('');
+  const [redirect,setRedirect] = useState('');
+  const {user} = useContext(UserContext);
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+    }
+  }, [user]);
+
+  let numberOfNights = 0;
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+  }
+
+  async function bookThisPlace() {
+      try { 
+          const response = await axios.post('/bookings', {
+              
+            checkIn,checkOut,numberOfGuests,name,phone,
+            place:place._id,
+            price:numberOfNights * place.price,
+        });
+        const bookingId = response.data._id;
+        toast('📑 Room have been booked!', {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
+        setRedirect(`/account/bookings/${bookingId}`);
+          
+      } catch (err) {
+        toast.error(err.response.data.message, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
+          if (err.response.status === 401) setRedirect('/login');
+      }
+    
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />
+  }
+
+  return (
+    <div className="bg-white shadow p-4 rounded-2xl">
+      <div className="text-2xl text-center">
+        Price: ${place.price} / per night
+      </div>
+      <div className="border rounded-2xl mt-4">
+        <div className="flex">
+          <div className="py-3 px-4 max-w-[50%] flex flex-col">
+            <label>Check in:</label>
+            <input type="date"
+                   value={checkIn}
+                   onChange={ev => setCheckIn(ev.target.value)}/>
+          </div>
+          <div className="py-3 px-4 border-l max-w-[50%] flex flex-col">
+            <label>Check out:</label>
+            <input type="date" value={checkOut}
+                   onChange={ev => setCheckOut(ev.target.value)}/>
+          </div>
+        </div>
+        <div className="py-3 px-4 border-t">
+          <label>Number of guests:</label>
+          <input type="number"
+                 value={numberOfGuests}
+                 onChange={ev => setNumberOfGuests(ev.target.value)}/>
+        </div>
+        {numberOfNights > 0 && (
+          <div className="py-3 px-4 border-t">
+            <label>Your full name:</label>
+            <input type="text"
+                   value={name}
+                   onChange={ev => setName(ev.target.value)}/>
+            <label>Phone number:</label>
+            <input type="tel"
+                   value={phone}
+                   onChange={ev => setPhone(ev.target.value)}/>
+          </div>
+        )}
+      </div>
+      <button onClick={bookThisPlace} className="g-primary mt-4">
+        Book this place
+        {numberOfNights > 0 && (
+          <span> ${numberOfNights * place.price}</span>
+        )}
+      </button>
+    </div>
+  );
+}
