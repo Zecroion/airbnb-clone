d40563978792237fed83fe136e6cac851b826019
Hussein Samy <hussein.samy02@gmail.com>
2023-06-18 17:53:01 +0200

    Adds footer component

---
+++ client/src/components/footer/footer.component.jsx
@@ -0,0 +1,10 @@
+import './footer.styles.scss'
+const Footer = () => {
+    return (
+        <div className="footer-container">
+        <p>Depeloped By <a className="link" href="https://github.com/husseinSamy" target="_blank" rel="noreferrer">Hussein</a></p>
+        </div>
+    )
+}
+
+export default Footer;
